549a4a86b4cde8d3627e36ac52276d177f05d90d
GESTER.SAMSON <21800719@usc.edu.ph>
2025-03-03 22:23:53 +0800

    wonderhoy

---
+++ TaskioApp/WelcomePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace TaskioApp
+{
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnStartClicked(object sender, EventArgs e)
+        {
+            // Navigate to MainPage (or another page)
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
