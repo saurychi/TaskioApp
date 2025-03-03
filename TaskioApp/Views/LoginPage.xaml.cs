b16d04773ace1d6d530ab6dab3160449035b0170
FrancisAustria <austriafrancisdaniel@gmail.com>
2025-03-03 23:08:03 +0800

    Added login page with no background image yet

---
+++ TaskioApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace TaskioApp.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnLoginClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Login", "Login button clicked!", "OK");
+        }
+    }
+}
